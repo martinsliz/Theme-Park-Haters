6645ef866bc7a3c17b6b8cd59e9272e0f03a2f3a
Freddy Caione <fac299@nyu.edu>
2023-02-21 14:42:28 -0500

    fix index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 const root = createRoot(document.getElementById('root'))
-
-
-  root.render(
-  <BrowserRouter/>
+root.render(
+  <BrowserRouter>
     <App />
-  <BrowserRouter/>
+  </BrowserRouter>
 )
