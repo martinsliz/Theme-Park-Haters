5b3fb82a835e2001703afae77e9def9b31c02d9f
Freddy Caione <fac299@nyu.edu>
2023-02-21 15:47:30 -0500

    parks.jsx maps over parks, creating parkCards

--- client/src/Pages/Parks.jsx
+++ client/src/Pages/Parks.jsx
@@ -1,6 +1,29 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import ParkCard from "../Components/ParkCard"
+
 const Parks = () => {
+
+    const [parks, setParks] = useState([])
+
+    const getAllParks = async () => {
+        const res = axios.get(`/api/parks`)
+        console.log(res)
+        setParks(res)
+    }
+
+    useEffect(() => {
+        getAllParks()
+    },[])
+
   return (
-    <div>Parks Listed Here</div>
+    <div className="parks-container">
+        {parks && (
+            parks.map(park => {
+                <ParkCard {...park}/>
+            })
+        )}
+    </div>
   )
 }
 export default Parks
