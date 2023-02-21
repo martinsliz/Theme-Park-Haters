1cd6511da284dd12672142508288cd4c8647c926
Liz Martins <elizabeth.martins5@gmail.com>
2023-02-21 16:25:34 -0500

    about, nav, globals correction

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,5 +1,13 @@
 const About = () => {
-  return <div>About</div>
+  return (
+    <div>
+      About
+      <div>
+        Here at Theme Park Haters Inc., we were tired of visiting sub-par theme
+        parks and we decided to let people know about it.
+      </div>
+    </div>
+  )
 }
 
 export default About
--- server.js
+++ server.js
@@ -2,19 +2,16 @@ const express = require('express')
 const routes = require('./routes')
 const db = require('./db')
 const logger = require('morgan')
-
-// require() imports and middleware here ^ ///////
-
 const PORT = process.env.PORT || 3001
 
 const app = express()
+// require() imports and middleware here ^ ///////
 
 app.use(logger('dev'))
 app.use(express.json())
+app.use('/api', routes)
 
 // app.use() middleware here ^ ///////////////////
-
-app.use('/api', routes)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
