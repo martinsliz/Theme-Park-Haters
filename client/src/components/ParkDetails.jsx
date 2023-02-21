66aebabf28548bc283b9983f082f6a02c0364870
Liz Martins <elizabeth.martins5@gmail.com>
2023-02-21 17:50:41 -0500

    rendered park details

--- client/src/components/ParkDetails.jsx
+++ client/src/components/ParkDetails.jsx
@@ -1,21 +1,36 @@
-import { useState, usEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { BASE_URL } from '../globals'
 import axios from 'axios'
 
-const ParkDetails = (props) => {
+const ParkDetails = () => {
   const [parkDetails, setParkDetails] = useState([])
 
   let { id } = useParams()
 
   const getParkDetails = async () => {
-    const response = await axios.get(`${BASE_URL}/parks/:id`)
-    setDetails(response.data)
-    console.log(response)
+    const response = await axios.get(`${BASE_URL}/parks/${id}`)
+    setParkDetails(response.data.park)
+    console.log(response.data)
   }
-  getParkDetails()
+  useEffect(() => {
+    getParkDetails()
+  }, [])
 
-  return <div>ParkDetails</div>
+  return (
+    <div>
+      <section className="imageContainer">
+        <img src={parkDetails.image} alt="" />
+        <div className="info">
+          <h3>Name: {parkDetails.name} </h3>
+          <h3>Location: {parkDetails.location} </h3>
+          <h3>Rating: {parkDetails.rating} </h3>
+          <h3>Open: {parkDetails.open} </h3>
+        </div>
+      </section>
+      <section></section>
+    </div>
+  )
 }
 
 export default ParkDetails
