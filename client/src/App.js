f986f5c9471fed142abf88489bfecab4c38448d6
Freddy Caione <fac299@nyu.edu>
2023-02-21 14:57:38 -0500

    App.js routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import { Route, Routes } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+// import { useState, useEffect } from 'react'
+// import axios from 'axios'
 import './App.css'
+import Home from './Pages/Home'
+import ParkDetails from "./Components/ParkDetails"
+import About from './Components/About'
+import Nav from "./Components/Nav"
+import PostForm from "./Components/PostForm"
 
 function App() {
-  return <div className="App"></div>
+  return <div className="App">
+    <Nav />
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/park/:id" element={<ParkDetails />}/>
+      <Route path="/park/:id/post" element={<PostForm />}/>
+    </Routes>
+
+  </div>
 }
 
 export default App
