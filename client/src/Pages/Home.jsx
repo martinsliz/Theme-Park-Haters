4ca1a757fef1166925fd9326fe8445f6085c1346
Freddy Caione <fac299@nyu.edu>
2023-02-22 10:18:39 -0500

    header photo styling

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,13 +1,15 @@
 import Parks from './Parks'
 import seasideFire from '../Images/seasideFire.jpeg'
+import abandoned from "../Images/abandoned.png"
+import abandonedCoaster from "../Images/abandonedCoaster.jpeg"
 import '../Styles/Home.css'
 
 const Home = () => {
   return (
     <>
-      <div className="home">Welcome to the most hated theme parks!</div>
-      <div>
-        <img src={seasideFire} alt="" />
+      <div className='header-photo-container' style={{backgroundImage: `url(${abandonedCoaster})`}}>
+        {/* <img className="header-photo" src={{style=}} alt="" /> */}
+        <h1 className='header-text'>Welcome to the most hated theme parks</h1>
       </div>
       <Parks />
     </>
