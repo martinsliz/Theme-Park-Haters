b0a904732fd5eef515e165b76ef3d6de86da313a
Sarah Maher <119453789+mahers12@users.noreply.github.com>
2023-02-22 09:42:10 -0700

    updated README page

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ router.get('/', (req, res) => res.send('This is root!'))
 router.post('/park', controllers.createPark)
 router.get('/parks', controllers.getAllParks)
 router.get('/parks/:id', controllers.getParkById)
-router.post('/post/:id', controllers.createPost)
-router.get('/posts/:id', controllers.deletePost)
+router.post('/post', controllers.createPost)
+router.delete('/posts/:id', controllers.deletePost)
 router.get('/parks/post', controllers.getPostsForPark)
 
 module.exports = router
