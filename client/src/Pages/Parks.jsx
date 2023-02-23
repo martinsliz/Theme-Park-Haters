6aba2bdf57e0865a88b71641fa2eb87180f2cd8c
Freddy Caione <fac299@nyu.edu>
2023-02-23 12:10:59 -0500

    lowercase

--- client/src/Pages/Parks.jsx
+++ client/src/Pages/Parks.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
 import ParkCard from "../Components/ParkCard"
-import "../Styles/Parks.css"
+import "../styles/Parks.css"
 
 const Parks = () => {
 
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import "../Styles/Nav.css"
+import "../styles/Nav.css"
 
 const Nav = () => {
   return (
